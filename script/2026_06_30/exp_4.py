a7ae6f6e80cfa974eb56ff6be7ab1c359ed296c3
Caleb Ju <calebju4@gatech.edu>
2026-07-01 11:50:11 -0400

    Longer runtimes for full run

--- script/2026_06_30/exp_4.py
+++ script/2026_06_30/exp_4.py
@@ -24,7 +24,7 @@ def setup_setting_files(seed_0, n_seeds, n_iters, print_info, skip_save=False):
     od["update_rule"] = int(pmd.Update.TSALLIS_UPDATE)
     od["n_iters"] = 1_000
     od["max_obs"] = math.inf
-    od["max_runtime_in_sec"] = 900
+    od["max_runtime_in_sec"] = 3600
     od["no_validation_gamma"] = True
 
     env_name_arr = ["discrete_inventory"]
